6b447b8e1e1f0d07c74fcfff5405f152879c9c3c
haider <haideralimughal512@gmail.com>
2024-01-05 10:45:57 +0500

    user page added

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,30 @@
 import React from "react"
 
-const UserPage = () => {
-  return <div>Users Page</div>
+interface User {
+  id: number
+  name: string
+}
+
+const UserPage = async () => {
+  //frequently change data cache no store
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+
+  //revalidate after 10 second it is only availabel in fetch function not in 3rd party labraries
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+  //     next: { revalidate: 10 },
+  //   })
+  const userList: User[] = await res.json()
+  return (
+    <>
+      <h1>Users</h1>
+      <code>{new Date().toLocaleTimeString()}</code>
+      <ul>
+        {userList.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default UserPage
