d5683ddba576f7b96c5fc87e53aeb16a40c1f81f
haider_ali <innoxentmughalforever@gmail.com>
2024-01-21 23:16:51 +0500

    oath applied in app

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,7 @@ const NavBar = () => {
   return <div className="flex bg-slate-200 p-5 mb-2">
     <Link href="/" className="mr-5">Next.js</Link>
     <Link href="/users" className="mr-5">User</Link>
+    <Link href="/api/auth/signin" className="mr-5">Login</Link>
   </div>;
 };
 
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,23 +1,39 @@
 "use client";
 import React, { useState } from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
   const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget
-      uploadPreset="who3rdcr"
-      onUpload={(result, widget) => {
-        console.log(result);
-        setPublicId("")
-      }}
-    >
-      {({ open }) => (
-        <button className="btn btn-primary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      <CldUploadWidget
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles: 5,
+        }}
+        uploadPreset="who3rdcr"
+        onUpload={(result, widget) => {
+          console.log(result);
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt=""></CldImage>
       )}
-    </CldUploadWidget>
+    </>
   );
 };
 
